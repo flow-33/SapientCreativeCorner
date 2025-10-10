44c56ea95d77f8edb021fdf63e62bc0901e40d48
Flow Fournier <flofourn@WKMZT09AA202.global.publicisgroupe.net>
2025-10-10 16:04:35 +0400

    feat: initial VirtualOS scaffold, galleries, weekly refresh, auto-reload, styling

---
+++ src/preload.js
@@ -0,0 +1,3 @@
+// See the Electron documentation for details on how to use preload scripts:
+// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+// Intentionally minimal; we will expose IPC in future phases for phone control
